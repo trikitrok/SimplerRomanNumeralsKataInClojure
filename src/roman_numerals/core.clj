f17e1a3d691c2861ed9c3e728f57433eb1a06069
trikitrok <trikitrok@gmail.com>
2014-12-14 20:54:26 +0100

    Made duplication more explicit

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -11,7 +11,7 @@
         "V" 
         (to-roman (- decimal 5)))
       
-      (cond
-        (= decimal 1) "I"
-        (= decimal 2) "II"
-        (= decimal 3) "III"))))
+      (if (>= decimal 1)
+        (str 
+          "I" 
+          (to-roman (- decimal 1)))))))
